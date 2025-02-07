c20bbf9cdaddf102fb84e572fd66e39558817a38
sarthakarora108108 <sarthakarora108108@gmail.com>
2025-02-07 19:02:42 +0530

    audio transcription api updated

--- command_recon.py
+++ command_recon.py
@@ -1,15 +1,23 @@
 import speech_recognition as sr
+from openai import OpenAI
 
 recogniser = sr.Recognizer()
+convertor = OpenAI()
 
 def command_recon():
     with sr.Microphone() as source:
-        recogniser.adjust_for_ambient_noise(source, duration = 0.7)
+        recogniser.adjust_for_ambient_noise(source, duration = 0.5)
         print("listening...")
 
         try:
             audio = recogniser.listen(source, timeout = 0.5, phrase_time_limit=0.5)
-            command = recogniser.recognize_google(audio).lower()
+            print("Auio is captured : processing...")
+
+            command = convertor.audio.transcription.create(
+                model = 'whisper-1',
+                file = audio
+            )
+            print('command recognised, command : $s', command)
             return command
         except sr.UnknownValueError:
             return None
